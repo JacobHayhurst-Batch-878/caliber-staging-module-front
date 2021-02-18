cb7b0eb1b0e04d3c332548162a2062ae36be193d
Jay Monari <jaymonari@outlook.com>
2021-02-18 09:27:03 -0500

    small band-aid for toast service and fixed bug of not being able to delete a swot in view-swot component

--- src/app/components/view-swot/view-swot.component.ts
+++ src/app/components/view-swot/view-swot.component.ts
@@ -199,7 +199,8 @@ export class ViewSwotComponent implements OnInit {
    * This method sends a request to the backend to delete a swot with id=id.
    */
   deleteSwot() {
-    this.swotService.deleteSwot(this.currentSwotAnalysis.id).subscribe();
-    this.router.navigate(['/home']);
+    this.swotService.deleteSwot(this.currentSwotAnalysis.id).subscribe(() => {
+      this.router.navigate(['/home']);
+    });
   }
 }
