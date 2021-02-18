08976f4fde84539033051d0fb30a5c472b1e46ab
Jay Monari <jaymonari@outlook.com>
2021-02-17 22:52:53 -0500

    refactored login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,8 +1,13 @@
 import { LoginService } from './../../services/login-service/login.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +16,8 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  isEmailError: boolean = false;
+  isPasswordError: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -28,15 +35,40 @@ export class LoginComponent implements OnInit {
 
   async loginUser() {
     const manager = this.loginForm.value;
-    const userCredentials = await this.ngFireAuth.signInWithEmailAndPassword(
-      manager.email,
-      manager.password
-    );
-    this.loginService
-      .getManagerId(userCredentials.user.email)
-      .subscribe((managerId) => {
-        window.sessionStorage.setItem('managerId', managerId);
-        this.router.navigate(['home']);
+    const userCredentials = await this.ngFireAuth
+      .signInWithEmailAndPassword(manager.email, manager.password)
+      .catch((err: HttpErrorResponse) => {
+        this.giveUserFeedback(err.message);
+        return null;
       });
+    if (userCredentials) {
+      this.loginService
+        .getManagerId(userCredentials.user.email)
+        .subscribe((managerId) => {
+          window.sessionStorage.setItem('managerId', managerId);
+          this.router.navigate(['home']);
+        });
+    }
+  }
+
+  private giveUserFeedback(errorMessage: string) {
+    const isPasswordWrong: boolean = errorMessage.includes('password');
+    const emailEl: HTMLInputElement = document.querySelector('#email');
+    const passwordEl: HTMLInputElement = document.querySelector('#password');
+    if (isPasswordWrong) {
+      this.isPasswordError = true;
+      passwordEl.style.border = '1px solid red';
+      passwordEl.style.boxShadow = '0px 0px 5px red';
+      this.isEmailError = false;
+      emailEl.style.border = '';
+      emailEl.style.boxShadow = '';
+    } else {
+      this.isPasswordError = false;
+      passwordEl.style.border = '';
+      passwordEl.style.boxShadow = '';
+      this.isEmailError = true;
+      emailEl.style.border = '1px solid red';
+      emailEl.style.boxShadow = '0px 0px 5px red';
+    }
   }
 }
